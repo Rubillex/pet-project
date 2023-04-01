10d737fc460184450297a7109fad36e107127360
kirill <kiruhak2005@gmail.com>
2023-04-01 22:41:54 +0700

    commit message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,73 @@
 <template>
-  <RouterView />
+
+  <div class="header">
+    <div class="content">
+      <img class="header__logo" src="src/assets/logo.svg" alt="image"/>
+      <a class="header__logo-link" href="" > Vite App </a>
+      <div class="header-right">
+        <a href="#" >Корзина</a>
+        <a href="" >Оформление</a>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="content">
+    <RouterView/>
+  </div>
+
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
 </script>
+
+<style scoped lang="scss">
+
+  .header {
+    width: 100vw;
+    overflow: hidden;
+    background-color: #f1f1f1;
+    padding: 10px 5px;
+    margin-bottom: 50px;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+
+    &__logo {
+      height: 45px;
+      float: left;
+
+      &-link {
+        font-size: 25px;
+        font-weight: bold;
+      }
+    }
+  }
+
+  .header a {
+    float: left;
+    color: black;
+    text-align: center;
+    padding: 12px;
+    text-decoration: none;
+    font-size: 18px;
+    line-height: 25px;
+    border-radius: 4px;
+  }
+
+  .header a:hover {
+    background-color: #ddd;
+    color: black;
+  }
+
+  .header-right {
+    float: right;
+  }
+
+
+  .content {
+    max-width: 1440px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,74 @@
 <template>
-    <div v-for="product in allProducts">
+
+
+
+  <div class="card__wrapper">
+    <div class="card" @click="openProductPage(product.id)" v-for="product in allProducts" :key="product.id">
+      <img class="card__image untouchable" :src="product.image" alt="image" />
       <span v-html="product.name" />
-      <span v-html="product.description" />
-      <span v-html="product.price" />
-      <img :src="product.image" />
+      <span class="card__price" v-html="convertPrice(product.price) + ' ₽'" />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { productStore } from "@/stores/productStore";
 import { storeToRefs } from "pinia";
+import {useRouter} from "@/use/router";
+import { useNumberDelimiter } from '@/use/number';
 
 const store = productStore();
 
 const { allProducts } = storeToRefs(store);
+const { router } = useRouter();
+const convertPrice = (price: number) => useNumberDelimiter(price);
+
+const openProductPage = (id: number) => {
+  router.push({
+    name: 'show',
+    params: {
+      itemId: id
+    }
+  })
+};
 </script>
+
+<style scoped lang="scss">
+
+
+  .card {
+    padding: 12px;
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    border-radius: 8px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    cursor: pointer;
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+
+    &__price {
+      padding-top: 10px;
+      font-weight: 700;
+      font-size: 21px;
+    }
+
+    &__image {
+      border-radius: 3px;
+      width: 100%;
+      height: 100%;
+      align-self: end;
+    }
+
+    &__wrapper {
+      display: grid;
+      padding: 40px;
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+      gap: 20px 50px;
+      background-color: #ffffff;
+    }
+  }
+
+</style>
