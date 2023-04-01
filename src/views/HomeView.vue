1907804793c18ad0b767f06ddbf1261e677ee615
Vadim <vadim.gubchenko@cyberia.studio>
2023-04-01 15:04:45 +0700

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-for="product in allProducts">
+      <span v-html="product.name" />
+      <span v-html="product.description" />
+      <span v-html="product.price" />
+      <img :src="product.image" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { productStore } from "@/stores/store";
+import { storeToRefs } from "pinia";
+
+const store = productStore();
+
+const { allProducts } = storeToRefs(store);
+</script>
